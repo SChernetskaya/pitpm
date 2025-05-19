a9f42a6cd2ccdbaddb0870cd969d38ba9693eb0b
Alena Rekunkova <alenarekunkova3017@gmail.com>
2025-05-19 11:54:37 +0300

    Com 3

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,25 +2,40 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
+from pygame.sprite import Group
+from game_stats import GameStats
+
 def run_game():
+
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
     bg_color = (230, 230, 230)
+    alien = Alien(ai_settings, screen)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     while True:
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
         ship.blitme()
         screen.fill(ai_settings.bg_color)
         ship.blitme()
         pygame.display.flip()
+        
 run_game()
